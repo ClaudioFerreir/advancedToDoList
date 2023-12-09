51b43c3df1738eb9840fe72129169f7bbd8d40bd
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2023-12-09 00:03:44 -0300

    initial setup

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobal = createGlobalStyle`
+  * {
+    margin: o;
+    padding: 0;
+    box-sizing: border-box;
+    family-font: 'Roboto', sans-serif;
+  }
+`
+
+export default EstiloGlobal
